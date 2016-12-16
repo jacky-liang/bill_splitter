5573c0c8a97775e5c6b1d4dc8575701772f2355b
Jacky Liang <jackyliang@berkeley.edu>
2016-12-16 18:35:21 +0000

    working

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
 
-    
+    $('#submit').click(function(){
+      var total = parseFloat($('#total').val());
+      var subtotal = parseFloat($("#subtotal").val());
+      var item = parseFloat($("#item").val());
+
+      var pay = item / subtotal * total;
+
+      console.log(pay);
+      $("#item_amount").html(pay);
+    });
 
 });
